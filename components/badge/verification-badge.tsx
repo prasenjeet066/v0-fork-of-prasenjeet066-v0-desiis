c36d461af0e0f4a6b44c3d91c073a5d0924aaf54
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-06-24 21:06:43 +0600

    Update verification-badge.tsx

--- components/badge/verification-badge.tsx
+++ components/badge/verification-badge.tsx
@@ -21,7 +21,7 @@ const VerificationBadge: React.FC<VerificationBadgeProps> = ({
   verified = true,
   label = "Verified",
   size = 24,
-  fill = "#ffff", // Default fill color
+  fill = "#0000", // Default fill color
   className = "",
 }) => {
   if (!verified) return null;
@@ -33,7 +33,7 @@ const VerificationBadge: React.FC<VerificationBadgeProps> = ({
       aria-label={label}
       data-testid="verification-badge"
     >
-      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg viewBox="0 0 24 24" fill="none">
         {/* Removed <defs> with linearGradient */}
         <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
         <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
