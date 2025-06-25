d664874bb7774a2b23fd1aebe48b52368445e817
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-06-25 18:08:16 +0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,9 @@ const config: Config = {
   ],
   theme: {
   	extend: {
+        fontFamily: {
+        racing: ['"Racing Sans One"'],
+        },
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
