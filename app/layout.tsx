74aafa5ec950b7787adab5d09da869d6e4396dc0
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-06-26 10:19:20 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+Arabic:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <script type="text/javascript" src="dist/purify.js"></script>
       </head>
       <body className={inter.className}>
         <LanguageProvider>{children}</LanguageProvider>
